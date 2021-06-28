0611d665a1cbf6bfa9c85f129f89481fe31621d9
clintonksang <sangclint0@gmail.com>
2021-06-28 10:08:06 +0300

    Update: Header details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Graphic Design`,
+    description: `Get the best Graphic Design, UI/UX and Digital Marketing services.`,
     author: `@gatsbyjs`,
   },
   plugins: [
